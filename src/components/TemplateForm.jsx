d230555abe8431a238b109f0a29637c7b3d4af0c
Mallikarjuna Reddy Nimmakayala <mallikarjunareddy.nimmakayala@gmail.com>
2023-11-26 23:29:01 +0530

    added disabled for Generate Project button

--- src/components/TemplateForm.jsx
+++ src/components/TemplateForm.jsx
@@ -258,6 +258,7 @@ const TemplateForm = () => {
                 <button
                     type="submit"
                     className="flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    disabled={isLoading}
                 >
                     { isLoading && <svg class="mr-3 h-5 w-5 animate-spin text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                         <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
