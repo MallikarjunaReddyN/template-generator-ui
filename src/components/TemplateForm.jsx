c5f623c45354381e614da7e2c23a699467b4850d
Mallikarjuna Reddy Nimmakayala <mallikarjunareddy.nimmakayala@gmail.com>
2023-11-27 21:38:12 +0530

    updated button background

--- src/components/TemplateForm.jsx
+++ src/components/TemplateForm.jsx
@@ -264,14 +264,14 @@ const TemplateForm = () => {
                 </button>
                 <button
                     type="submit"
-                    className="flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    className={`flex items-center rounded-md ${isLoading ? 'bg-indigo-300' : 'bg-indigo-600'} px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600`}
                     disabled={isLoading}
                 >
                     { isLoading && <svg class="mr-3 h-5 w-5 animate-spin text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                         <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
                         <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                     </svg>} 
-                    {isLoading ? 'Generating...' :'Generate Project' }
+                    {isLoading ? 'Generating...' : 'Generate Project' }
                 </button>
             </div>
         </form>
