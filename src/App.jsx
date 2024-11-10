0e11f57adbd4240ccc5386e439cd8d4e7a340807
Mallikarjuna Reddy Nimmakayala <mallikarjunareddy.nimmakayala@gmail.com>
2024-11-10 20:58:00 +0530

    adjusted view

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-      <div className="container mx-auto">
+      <div className="container mx-auto h-screen">
       <p className="text-xl md:text-3xl text-indigo-600 font-bold mb-[30px]">
           Spring Boot Template Generator
       </p>
