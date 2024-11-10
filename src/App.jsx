5c392124d14c283dd9eba19218cef68de28c96d3
Mallikarjuna Reddy Nimmakayala <mallikarjunareddy.nimmakayala@gmail.com>
2024-11-10 22:09:51 +0530

    adjusted view

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <div className="container mx-auto min-h-screen">
-      <p className="text-xl md:text-3xl text-indigo-600 font-bold mb-[30px]">
+      <p className="text-xl md:text-3xl text-indigo-600 font-bold pt-6 mb-[30px]">
           Spring Boot Template Generator
       </p>
       <TemplateForm />
